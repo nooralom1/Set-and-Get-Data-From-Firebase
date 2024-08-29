cfeb49bee28b6026763612c9a8e19a9c679b08d6
NS NooR <non377938@gmail.com>
2024-08-30 02:08:24 +0600

    add

--- lib/view/screen/add_info/add_info.dart
+++ lib/view/screen/add_info/add_info.dart
@@ -1,7 +1,9 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:set_and_get_data_from_firebase/controller/getx_controller/add_info.dart';
-import 'package:set_and_get_data_from_firebase/view/widget/common_textfield.dart';
+import 'package:set_and_get_data_from_firebase/view/common_widget/common_button.dart';
+import 'package:set_and_get_data_from_firebase/view/common_widget/common_textfield.dart';
 
 class AddInfo extends StatelessWidget {
   const AddInfo({super.key});
@@ -11,25 +13,48 @@ class AddInfo extends StatelessWidget {
     AddInfoController controller = Get.put(AddInfoController());
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Add Info"
-        ),
+        title: Text("Add Info"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          CommonTextField(hintText: 'Name', controller: controller.nameController,),
-          CommonTextField(hintText: 'Id', controller: controller.idController,),
-          CommonTextField(hintText: 'Course Name', controller: controller.courseController,),
-          CommonTextField(hintText: 'Institute', controller: controller.instituteController,),
-          CommonTextField(hintText: 'Department', controller: controller.depController,),
-          Row(
-            children: [
-              CommonTextField(hintText: 'Join Date', controller: controller.joinDateController),
-              CommonTextField(hintText: 'Validity', controller: controller.validityController,),
-            ],
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CommonTextField(
+              hintText: 'Name',
+              controller: controller.nameController,
+            ),
+            CommonTextField(
+              hintText: 'Id',
+              controller: controller.idController,
+            ),
+            CommonTextField(
+              hintText: 'Course Name',
+              controller: controller.courseController,
+            ),
+            CommonTextField(
+              hintText: 'Institute',
+              controller: controller.instituteController,
+            ),
+            CommonTextField(
+              hintText: 'Department',
+              controller: controller.depController,
+            ),
+            CommonTextField(
+                hintText: 'Join Date',
+                controller: controller.joinDateController),
+            CommonTextField(
+              hintText: 'Validity',
+              controller: controller.validityController,
+            ),
+            SizedBox(height: 40,),
+            CommonButton(
+              buttonName: 'GET INFO',
+              onTap: () {
+                log("message");
+              },
+            )
+          ],
+        ),
       ),
     );
   }
