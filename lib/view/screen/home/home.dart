9e241b2cfa690fb475f159e765c4adf8a8dcf2d2
NS NooR <non377938@gmail.com>
2024-08-30 02:08:46 +0600

    add

--- lib/view/screen/home/home.dart
+++ lib/view/screen/home/home.dart
@@ -1,12 +1,38 @@
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:set_and_get_data_from_firebase/view/common_widget/common_button.dart';
+import 'package:set_and_get_data_from_firebase/view/screen/add_info/add_info.dart';
+import 'package:set_and_get_data_from_firebase/view/screen/home/widget/infoviewcard.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return const InfoViewCard(
+              name: 'Name: ',
+              id: 'Id: ',
+              course: 'Course Name: ',
+              institute: 'Institute: ',
+              department: 'Department: ',
+              join: 'Join Date: ',
+              validity: 'Validity: ',
+            );
+          }),
+      bottomNavigationBar: BottomAppBar(
+        child: CommonButton(buttonName: "ADD INFO", onTap: (){
+          Get.to(()=>const AddInfo());
+        }),
+      ),
     );
   }
 }
