0812239811834bc82f7e96e77743e8eee317aef5
NS NooR <non377938@gmail.com>
2024-08-29 23:23:46 +0600

    firebase

---
+++ lib/controller/getx_controller/add_info.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class AddInfoController extends GetxController{
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController idController = TextEditingController();
+  TextEditingController courseController = TextEditingController();
+  TextEditingController instituteController = TextEditingController();
+  TextEditingController depController = TextEditingController();
+  TextEditingController joinDateController = TextEditingController();
+  TextEditingController validityController = TextEditingController();
+}
