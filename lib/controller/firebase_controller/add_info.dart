e7ec6cda196c9f2c19b49e3752534a35661ab77f
NS NooR <non377938@gmail.com>
2024-08-30 17:45:03 +0600

    add

--- lib/controller/firebase_controller/add_info.dart
+++ lib/controller/firebase_controller/add_info.dart
@@ -1,9 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+
 
 class AddInfoService{
 
   static Future<bool> addInfoService({required Map<String,dynamic>info})async{
     try{
+      FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+      await firebaseFirestore.collection("User Info").add(info);
+      return true;
     }catch(e){
       debugPrint("Error: $e");
     }
