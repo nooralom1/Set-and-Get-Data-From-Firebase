a1d0a3c74f79a8131a44b76f4bb6c5e1c1b6d7ce
NS NooR <non377938@gmail.com>
2024-08-30 17:45:06 +0600

    add

--- lib/controller/getx_controller/add_info.dart
+++ lib/controller/getx_controller/add_info.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:set_and_get_data_from_firebase/controller/firebase_controller/add_info.dart';
 
 class AddInfoController extends GetxController{
 
@@ -10,6 +13,8 @@ class AddInfoController extends GetxController{
   TextEditingController depController = TextEditingController();
   TextEditingController joinDateController = TextEditingController();
   TextEditingController validityController = TextEditingController();
+  RxBool isLoading = false.obs;
+  final formKey = GlobalKey<FormState>();
 
   addUserInfo()async{
     Map<String,dynamic> info ={
@@ -21,5 +26,10 @@ class AddInfoController extends GetxController{
       "join date": joinDateController.text,
       "validity": validityController.text
     };
+    isLoading.value = true;
+    log("================");
+    await AddInfoService.addInfoService(info: info);
+    log("=======11111111=========");
+    isLoading.value = false;
   }
 }
